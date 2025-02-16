280ab2d92ede756cf6b7a1b93ddc0724fae3ad9d
Axel Sanchez <axl9305@gmail.com>
2025-02-16 09:28:38 +0000

    Creating plot to show decision regions

--- irisdataset.py
+++ irisdataset.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import perceptron
+from matplotlib.colors import ListedColormap
 # %%
 df = pd.read_csv('http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',
                  header=None, encoding='utf-8')
@@ -38,4 +39,49 @@ plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
 plt.xlabel('Epochs')
 plt.ylabel('Number of updates')
 plt.show()
+# %%
+
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+    """
+    A function to visualize the decision boundaries for 2D datasets
+    """
+    # setup marker generator and color map
+    """"
+        Define a number of colors and markers and 
+        create a colormap from the list of colors via ListedColormap
+    """
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    """
+        Determine the minimum and maximum values for the two features
+        and use those feature vectors to create a pair of grid arrays
+    """
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_smax = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_smax, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # plot class samples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
+                    alpha=0.8, c=colors[idx],
+                    marker=markers[idx], label=cl, edgecolors='black')
+
+
+# %%
+plot_decision_regions(X, y, classifier=ppn)
+plt.xlabel('sepal length [cm]')
+plt.ylabel('petal length [cm]')
+plt.legend(loc='upper left')
+plt.show()
+
 # %%
