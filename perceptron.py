1c8b30ed2527fe100ad62919fa14521fb996b9a6
Axel Sanchez <axl9305@gmail.com>
2025-02-16 09:06:19 +0000

    Updating perceptron format. Fixing identation

--- perceptron.py
+++ perceptron.py
@@ -1,4 +1,6 @@
 import numpy as np
+
+
 class Perceptron(object):
     """Perceptron classifier.
     Parameters
@@ -9,7 +11,7 @@ class Perceptron(object):
         Passes over the training dataset.
     random_state : int
         Random number generator seed for random weight initialization.
-        
+
     Attributes
     -----------
     w_ : 1d-array
@@ -17,44 +19,55 @@ class Perceptron(object):
     errors_ : list
         Number of misclassifications (updates) in each epoch.
     """
+
     def __init__(self, eta=0.01, n_iter=50, random_state=1):
         self.eta = eta
         self.n_iter = n_iter
         self.random_state = random_state
 
-def fit(self, X, y):
-    """Fit training data.
-    Parameters
-    ----------
-    X : {array-like}, shape = [n_samples, n_features]
-        Training vectors, where n_samples is the number of samples and
-        n_features is the number of features.
-    y : array-like, shape = [n_samples]
-        Target values.
-    Returns
-    -------
-    self : object
-    """
-    rgen = np.random.RandomState(self.random_state)
-    self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
-    self.errors_ = []
+    def fit(self, X, y):
+        """Fit training data.
+        Parameters
+        ----------
+        X : {array-like}, shape = [n_samples, n_features]
+            Training vectors, where n_samples is the number of samples and
+            n_features is the number of features.
+        y : array-like, shape = [n_samples]
+            Target values.
+        Returns
+        -------
+        self : object
+        """
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
+        self.errors_ = []
 
-    for _ in range(self.n_iter):
-        errors = 0
-        for xi, target in zip(X, y):
-            update = self.eta * (target - self.predict(xi))
-            self.w_[1:] += update * xi
-            self.w_[0] += update
-            errors += int(update != 0.0)
-        self.errors_.append(errors)
+        for _ in range(self.n_iter):
+            errors = 0
+            for xi, target in zip(X, y):
+                update = self.eta * (target - self.predict(xi))
+                self.w_[1:] += update * xi
+                self.w_[0] += update
+                errors += int(update != 0.0)
+            self.errors_.append(errors)
 
-def net_input(self, X):
-    return np.dot(X, self.w_[1:]) + self.w_[0]
+        return self
 
-def predict(self, X):
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def predict(self, X):
         """"
-        This method helps to predict the class labels for the weight update.
-        Also it helps to predict the class labels for the input data (new data).
+            Predict class labels for samples in X.
+            Parameters
+            ----------
+            X : array-like, shape = [n_samples, n_features]
+                The input data.
+            Returns
+            -------
+            array, shape = [n_samples]
 
+            This method helps to predict the class labels for the weight update.
+            Also it helps to predict the class labels for the input data (new data).
         """
         return np.where(self.net_input(X) >= 0.0, 1, -1)
