68ac51c75a5a6053898fa7195381aeaad25350a8
Axel Sanchez <axl9305@gmail.com>
2025-02-17 20:54:01 +0000

    Creating example how Adaline works Implementing standaridization to improve gradient descent

--- irisdataset.py
+++ irisdataset.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import perceptron
+import adaline
 from matplotlib.colors import ListedColormap
 # %%
 df = pd.read_csv('http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',
@@ -82,6 +83,45 @@ plot_decision_regions(X, y, classifier=ppn)
 plt.xlabel('sepal length [cm]')
 plt.ylabel('petal length [cm]')
 plt.legend(loc='upper left')
+plt.show()
+
+# %%
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
+ada1 = adaline.Adaline(n_iter=10, eta=0.01).fit(X, y)
+ax[0].plot(range(1, len(ada1.cost_) + 1), np.log10(ada1.cost_), marker='o')
+ax[0].set_xlabel('Epochs')
+ax[0].set_ylabel('log(Sum-squared-error)')
+ax[0].set_title('Adaline - Learning rate 0.01')
+
+ada2 = adaline.Adaline(n_iter=10, eta=0.0001).fit(X, y)
+ax[1].plot(range(1, len(ada2.cost_) + 1), ada2.cost_, marker='o')
+ax[1].set_xlabel('Epochs')
+ax[1].set_ylabel('Sum-squared-error')
+ax[1].set_title('Adaline - Learning rate 0.0001')
+plt.show()
+# %%
+# Standardization
+x_std = np.copy(X)
+x_std[:, 0] = (X[:, 0] - X[:, 0].mean()) / X[:, 0].std()
+x_std[:, 1] = (X[:, 1] - X[:, 1].mean()) / X[:, 1].std()
+
+# %%
+ada = adaline.Adaline(n_iter=15, eta=0.01)
+ada.fit(x_std, y)
+
+plot_decision_regions(x_std, y, classifier=ada)
+plt.title('Adaline - Gradient Descent')
+plt.xlabel('sepal length [standardized]')
+plt.ylabel('petal length [standardized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
+
+plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Sum-squared-error')
+plt.tight_layout()
 plt.show()
 
 # %%
